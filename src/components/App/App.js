2383e9b70a2dc69157339609458acd3038da6591
Dillona25 <Dillyb00by49@gmail.com>
2023-10-17 17:18:18 -0500

    Updated the project structure

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import logo from "../../logo.svg";
+import "./App.css";
+import "../Header/Header.css";
+
+function App() {
+  return (
+    <div>
+      <header className="header"></header>
+    </div>
+  );
+}
+
+export default App;
