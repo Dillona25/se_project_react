57bd69e20d84d94e211dc59b5f015d8c5976eb23
Dillona25 <Dillyb00by49@gmail.com>
2023-10-17 19:35:12 -0500

    Added the Header and WeatherCard sections

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,45 @@
 import logo from "../../logo.svg";
 import "./App.css";
 import "../Header/Header.css";
+import "../WeatherCard/WeatherCard.css";
+import headerLogo from "../../images/wtwr.svg";
+import headerAvatar from "../../images/Avatar WTWR.svg";
+import cloud from "../../images/Cloud.svg";
+import sun from "../../images/Sun.svg";
+
+const currentDate = new Date().toLocaleString("default", {
+  month: "long",
+  day: "numeric",
+});
 
 function App() {
   return (
-    <div>
-      <header className="header"></header>
+    <div className="App">
+      <header className="header">
+        <div className="header__info">
+          <img src={headerLogo} alt="Logo" className="header__logo"></img>
+          <p className="header__info_date">{currentDate}, New York</p>
+        </div>
+        <div className="header__info_user">
+          <button className="header__button">+ Add New Clothes</button>
+          <p className="header__name">Terrence Tegegne</p>
+          <img
+            className="header__avatar"
+            alt="Users Avatar"
+            src={headerAvatar}
+          ></img>
+        </div>
+      </header>
+
+      <main>
+        <section className="weather">
+          <div className="weather__container">
+            <h1 className="weather__temp">75 F</h1>
+            <img className="weather__cloud" src={cloud} alt="cloud image"></img>
+            <img className="weather__sun" src={sun} alt="sun image"></img>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
